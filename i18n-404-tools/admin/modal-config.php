c3430f41c4d491ae51486e2aaf5e9f412d534aba
Eric van der Vlist <vdv@dyomedea.com>
2025-07-02 16:50:08 +0200

    Fixed incoherence between CSS and PHP

--- i18n-404-tools/admin/modal-config.php
+++ i18n-404-tools/admin/modal-config.php
@@ -3,14 +3,14 @@ global $i18n404tools_modal_config;
 
 $i18n404tools_modal_config = [
     // CSS classes
-    'action_class'   => 'i18n404tools-action',
-    'overlay_class'  => 'i18n404tools-modal-overlay',
-    'content_class'  => 'i18n404tools-modal-content',
-    'close_class'    => 'i18n404tools-modal-close',
-    'body_class'     => 'i18n404tools-modal-body',
+    'action_class'   => 'i18n-404-tools-action',
+    'overlay_class'  => 'i18n-404-tools-modal-overlay',
+    'content_class'  => 'i18n-404-tools-modal-content',
+    'close_class'    => 'i18n-404-tools-modal-close',
+    'body_class'     => 'i18n-404-tools-modal-body',
 
     // Data attribute names (prefix all!)
-    'data_command'   => 'data-i18n404tools-command',
-    'data_plugin'    => 'data-i18n404tools-plugin',
-    'data_step'      => 'data-i18n404tools-step',
+    'data_command'   => 'data-i18n-404-tools-command',
+    'data_plugin'    => 'data-i18n-404-tools-plugin',
+    'data_step'      => 'data-i18n-404-tools-step',
 ];
