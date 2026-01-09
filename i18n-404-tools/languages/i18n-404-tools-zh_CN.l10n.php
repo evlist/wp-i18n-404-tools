bcc950db982ea7c03d7b442b13e780c376dcd972
Eric van der Vlist <vdv@dyomedea.com>
2026-01-09 14:06:37 +0000

    Adding missing licenses

--- i18n-404-tools/languages/i18n-404-tools-zh_CN.l10n.php
+++ i18n-404-tools/languages/i18n-404-tools-zh_CN.l10n.php
@@ -1,4 +1,9 @@
 <?php
+
+// SPDX-FileCopyrightText: 2026 2025, 2026 Eric van der Vlist <vdv@dyomedea.com>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 /**
  * Chinese (Simplified) translation data.
  *
