2b3ac15e0ae6061e059e3bca71adcccbd4ee0321
Eric van der Vlist <vdv@dyomedea.com>
2026-01-09 08:38:52 +0000

    Installing imagemagick in the codespace for assets creation (#14)

---
+++ .devcontainer/sbin/start.sh.d/99-apt.local.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# SPDX-FileCopyrightText: 2025, 2026 Eric van der Vlist <vdv@dyomedea.com>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+sudo apt-get update 
+sudo apt-get install -y imagemagick
